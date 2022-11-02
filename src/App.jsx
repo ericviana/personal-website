e92aab8cc6eee537e978be8a38c727a080f1a0a2
Eric Viana <ericvsdefault@gmail.com>
2022-11-02 19:18:49 -0300

    Change dock bg and add title to icons

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,11 @@
-import { useState } from 'react';
 import { Dock } from './components/Dock/Dock.jsx';
-import Logo from '/home/eric/workspace/personal/eric.com/src/assets/logo.png'
 import './fonts/Magnat-TextRegular.ttf';
 import './fonts/Magnat-TextBold.ttf';
 import './fonts/Magnat-HeadBold.ttf';
 import './App.css';
 
 function App() {
-  const [count, setCount] = useState(0)
-
+  
   return (
     <div>
       <div className="hero">
--- src/components/Dock/Dock.jsx
+++ src/components/Dock/Dock.jsx
@@ -7,10 +7,10 @@ import About from '/home/eric/workspace/personal/eric.com/src/assets/icons/about
 export function Dock() {
   return (
     <div className={styles.container}>
-      <a href="#"><img className='icon' src={Home} alt="Home icon" /></a>
-      <a href="#"><img className='icon' src={Projects} alt="Project icon" /></a>
-      <a href="#"><img className='icon' src={Articles} alt="Article icon" /></a>
-      <a href="#"><img className='icon' src={About} alt="About icon" /></a>
+      <a href="#"><img className='icon' src={Home} alt="Home icon" title='home'/></a>
+      <a href="#"><img className='icon' src={Projects} alt="Project icon" title='projects'/></a>
+      <a href="#"><img className='icon' src={Articles} alt="Article icon" title='articles'/></a>
+      <a href="#"><img className='icon' src={About} alt="About icon" title='about me'/></a>
     </div>
   )
 }
