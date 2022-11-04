0ec73bdb8c1123f2a2174e1cc177dff09ca4d9f5
Eric Viana <ericvsdefault@gmail.com>
2022-11-04 20:25:34 -0300

    Change text font

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,4 @@
 import { Dock } from './components/Dock/Dock.jsx';
-import './fonts/Magnat-TextRegular.ttf';
-import './fonts/Magnat-TextBold.ttf';
-import './fonts/Magnat-HeadBold.ttf';
 import './App.css';
 
 function App() {
