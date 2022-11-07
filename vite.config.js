372e3ddfe2ad5f88c1d3ffb2256d89d71d4f1cba
Eric Viana <ericvsdefault@gmail.com>
2022-11-07 12:42:58 -0300

    Set enviromment and Add relative path

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,22 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()]
-})
+  plugins: [react()],
+  base: '',
+  resolve: {
+    alias: [{ find: '@', replacement: '/src' }],
+   },
+
+});
+
+/* import { defineConfig } from 'vite';
+import reactRefresh from '@vitejs/plugin-react-refresh';
+const path = require('path');
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  resolve: {
+    alias: [{ find: '@', replacement: path.resolve(__dirname, '/src') }],
+  },
+  plugins: [reactRefresh()],
+}); */
