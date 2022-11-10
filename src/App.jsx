d814b5cac1afc4cab267076d18a0f15a560a5283
Eric Viana <ericvsdefault@gmail.com>
2022-11-10 00:23:28 -0300

    Remove link from router import

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { Dock } from './components/Dock/Dock.jsx';
 import './App.scss';
-import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import About from "./pages/About";
 import Articles from "./pages/Articles";
