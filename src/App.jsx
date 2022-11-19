88da97de75e3094881bafdaf735ab68b87a7f7f4
Eric Viana <ericvsdefault@gmail.com>
2022-11-19 20:52:57 -0300

    Formating

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
-import { Dock } from './components/Dock/Dock.jsx';
-import './App.scss';
+import { Dock } from "./components/Dock/Dock.jsx";
+import "./App.scss";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import About from "./pages/About";
@@ -12,16 +12,16 @@ function App() {
     <div className="App" id="nav">
       <Router>
         <Routes>
-        <Route path='/' element={ <Home /> }/>
-        <Route path='/about' element={ <About /> }/>
-        <Route path='/articles' element={ <Articles /> }/>
-        <Route path='/projects' element={ <Projects /> }/>
-        <Route path='*' element={ <ErrorPage /> }/>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/articles" element={<Articles />} />
+          <Route path="/projects" element={<Projects />} />
+          <Route path="*" element={<ErrorPage />} />
         </Routes>
         <Dock />
       </Router>
     </div>
-  )
+  );
 }
 
 export default App;
