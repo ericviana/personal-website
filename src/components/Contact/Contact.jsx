837f2e1ba1839903cb1ce27d903b9bc3fa08edc6
Eric Viana <ericvsdefault@gmail.com>
2022-12-23 11:18:37 -0300

    Add links urls

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -5,15 +5,15 @@ function Contact() {
   return (
     <div className={styles.container}>
       <h1 className={styles.title}>Contact</h1>
-      <a href="#" className="method">
+      <a href="https://github.com/ericviana" className="method">
         Email
       </a>{" "}
       <span>• hello@ericviana.com.br</span> <br /> <br />
-      <a href="#" className="method">
+      <a href="mailto:hello@ericviana.com.br" className="method">
         Github
       </a>{" "}
       <span>• github.com/ericviana</span> <br /> <br />
-      <a href="#" className="method">
+      <a href="https://www.linkedin.com/in/eric-viana/" className="method">
         Linkedin
       </a>{" "}
       <span>• linkedin.com/in/eric-viana</span> <br /> <br />
