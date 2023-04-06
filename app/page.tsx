e4dc413312db7c24438e4060d5f9731abe41bc9d
Eric Viana <ericvsdefault@gmail.com>
2023-04-06 17:47:11 -0300

    Update colors

--- app/page.tsx
+++ app/page.tsx
@@ -12,25 +12,25 @@ export const metadata = {
 
 export default function Home() {
   return (
-    <>
-      <header className="m-6">
+    <div className='bg-blue-700 min-h-screen'>
+      <header className="p-4">
         <Image src={logo} alt="" className='hover:animate-spin'/>
       </header>
       <div className="mx-auto my-36 h-full w-full max-w-3xl rounded-xl px-4">
         <h1 className="mb-8 py-3 text-xl font-bold lowercase antialiased">
           My name is{' '}
-          <a href="" className="text-zinc-400 transition-all duration-300 hover:text-zinc-300">
+          <a href="" className="text-blue-400 transition-all duration-300 hover:text-blue-300">
             Eric
           </a>
           , I'm currently living in brazil. My{' '}
           <Link
             href={`/career`}
-            className="text-zinc-400 transition-all duration-300 hover:text-zinc-300"
+            className="text-blue-400 transition-all duration-300 hover:text-blue-300"
           >
             career
           </Link>{' '}
           is resolving around software engineering world-class
-          <Link href={`/projects`} className="text-zinc-400 transition-all duration-300 hover:text-zinc-300">
+          <Link href={`/projects`} className="text-blue-400 transition-all duration-300 hover:text-blue-300">
             {' '}
             projects
           </Link>{' '}
@@ -39,12 +39,12 @@ export default function Home() {
           things and learn through constant experimentation.
         </h1>
         <a
-          className="mt-14 rounded-xl border-[2.8px] border-b-[6px] border-zinc-700 bg-zinc-800 px-3 py-2 text-base font-bold antialiased transition-all hover:border-b-[2.8px]"
+          className="mt-14 rounded-xl border-[2.8px] border-b-[6px] border-blue-500 bg-blue-600 px-3 py-2 text-base font-black antialiased transition-all hover:border-b-[2.8px] text-white"
           href=""
         >
           Press ⌘ + K to navigate
         </a>
       </div>
-    </>
+    </div>
   );
 }
