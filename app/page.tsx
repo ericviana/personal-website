243fa09618fe636ba078f028a92dee9849a52f07
Eric Viana <ericvsdefault@gmail.com>
2023-09-14 18:10:17 -0300

    Fix link

--- app/page.tsx
+++ app/page.tsx
@@ -15,7 +15,7 @@ export default function Home() {
         <p className="text-neutral-500 text-sm mt-4">With each keystroke, I transmute algorithms into stories, emotions into visuals, and connections into immersive encounters. I embrace the responsibility of shaping how we interact with the digital landscape, infusing empathy, purpose, and beauty into every creation.</p>
         <div className="flex gap-2 mt-7">
           <a href="https://www.fros.dev" className="bg-zinc-900/90 text-xs text-neutral-50 shadow-lg hover:shadow-xl transition-all rounded-lg w-fit px-3 py-1 border border-gray-600">✨ Fros</a>
-          <a href="https://www.fros.dev" className="bg-zinc-900/90 text-xs text-neutral-50 shadow-lg hover:shadow-xl transition-all rounded-lg w-fit px-3 py-1 border border-gray-600">📖 Articles</a>
+          <a href="https://blog.fros.dev" className="bg-zinc-900/90 text-xs text-neutral-50 shadow-lg hover:shadow-xl transition-all rounded-lg w-fit px-3 py-1 border border-gray-600">📖 Articles</a>
           <a href="https://www.crafta.studio" className="bg-zinc-900/90 text-xs text-neutral-50 shadow-lg hover:shadow-xl transition-all rounded-lg w-fit px-3 py-1 border border-gray-600">🏅 Agency</a>
           <a href="#" className="bg-zinc-100/60 text-xs text-neutral-400/60 rounded-lg w-fit px-3 py-1 border border-gray-100 cursor-not-allowed" title="Comming soon">🏦 Venture Capital</a>
         </div>
