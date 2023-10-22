4ccd81ea023721371eb94d6b521ab781c5bb5d5d
Eric Viana <ericvsdefault@gmail.com>
2023-10-21 23:59:55 -0300

    Add cmd k

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import './globals.css';
 import { Inter } from 'next/font/google';
+import { ThemeProvider } from './components/theme-provider';
 
 const inter = Inter({
   weight: '400',
@@ -9,7 +10,14 @@ const inter = Inter({
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <ThemeProvider
+        attribute="class"
+        defaultTheme="system"
+        enableSystem
+        disableTransitionOnChange
+      >
+        <body className={inter.className}>{children}</body>
+      </ThemeProvider>
     </html>
   );
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,7 @@
+import { ModeToggle } from "./components/toggle-theme";
+import { CommandDialogDemo } from "./components/command-demo";
+import { DraftingCompass, Palette, Code2, KeyboardIcon, Heart, Shell } from "lucide-react"
+
 export const metadata = {
   title: 'Eric Viana',
   description:
@@ -9,6 +13,15 @@ export const metadata = {
 
 export default function Home() {
   return (
-    <div className="bg-[#050505] text-zinc-900 antialiased h-[100dvh]"></div>
+    <div className="antialiased h-[100dvh] p-4">
+      <div className="flex justify-between">
+        <br />
+        <ModeToggle />
+      </div>
+      <div className="max-w-xl mx-auto mt-[30vh] flex flex-col gap-6 tracking-wide font-semibold">
+        <h1 className="text-xl">I am Eric, an experienced design engineer <DraftingCompass className="inline"/> dedicated to crafting meaningful <Shell className="inline" /> experiences. In the realm where technology and aesthetics  converge, I wield code <KeyboardIcon className="inline"/> and design <Palette className="inline"/> as my tools to breathe life into the intangible.</h1>
+        <CommandDialogDemo />
+      </div>
+    </div>
   );
 }
