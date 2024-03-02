798e87b4c2e2bb3b454084d6602727ffefb958c7
Eric Viana <ericvsdefault@gmail.com>
2024-03-02 09:02:59 -0300

    add_work

---
+++ app/work/page.tsx
@@ -0,0 +1,61 @@
+import Image from 'next/image';
+
+const work = [
+  {
+    id: 1,
+    name: 'Work 1',
+    src: '/assets/1.png',
+  },
+  {
+    id: 2,
+    name: 'Work 2',
+    src: '/assets/2.png',
+  },
+  {
+    id: 3,
+    name: 'Work 3',
+    src: '/assets/3.png',
+  },
+  {
+    id: 4,
+    name: 'Work 4',
+    src: '/assets/4.png',
+  },
+  {
+    id: 5,
+    name: 'Work 5',
+    src: '/assets/5.png',
+  },
+  {
+    id: 6,
+    name: 'Work 6',
+    src: '/assets/6.png',
+  },
+  {
+    id: 7,
+    name: 'Work 7',
+    src: '/assets/7.png',
+  },
+  {
+    id: 8,
+    name: 'Work 8',
+    src: '/assets/8.png',
+  },
+  {
+    id: 9,
+    name: 'Work 9',
+    src: '/assets/9.png',
+  },
+];
+
+export default function Page() {
+  return (
+    <div className="bg-gradient-to-br from-[#1E1E1E] to-[#151515] pb-10">
+      {work.map((item) => (
+        <div key={item.id} className="max-w-xl mx-auto px-6 mt-6">
+          <Image src={item.src} alt={item.name} width={600} height={600} />
+        </div>
+      ))}
+    </div>
+  );
+}
