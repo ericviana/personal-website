93663f37c468dc7127325c9b48dfc800df55ce1b
Eric Viana <ericvsdefault@gmail.com>
2024-03-18 22:27:51 -0300

    remove linkedin

--- app/components/command-demo.tsx
+++ app/components/command-demo.tsx
@@ -1,6 +1,6 @@
-"use client"
+'use client';
 
-import * as React from "react"
+import * as React from 'react';
 import {
   Twitter,
   Wand,
@@ -9,10 +9,8 @@ import {
   Sparkles,
   TerminalSquare,
   Workflow,
-  Linkedin,
-  Github
-} from "lucide-react"
-
+  Github,
+} from 'lucide-react';
 
 import {
   CommandDialog,
@@ -23,31 +21,35 @@ import {
   CommandList,
   CommandSeparator,
   CommandShortcut,
-} from "@/components/ui/command"
+} from '@/components/ui/command';
 
 export function CommandDialogDemo() {
-  const [open, setOpen] = React.useState(false)
+  const [open, setOpen] = React.useState(false);
 
   React.useEffect(() => {
     const down = (e: KeyboardEvent) => {
-      if (e.key === "k" && (e.metaKey || e.ctrlKey)) {
-        e.preventDefault()
-        setOpen((open) => !open)
+      if (e.key === 'k' && (e.metaKey || e.ctrlKey)) {
+        e.preventDefault();
+        setOpen((open) => !open);
       }
-    }
+    };
 
-    document.addEventListener("keydown", down)
-    return () => document.removeEventListener("keydown", down)
-  }, [])
+    document.addEventListener('keydown', down);
+    return () => document.removeEventListener('keydown', down);
+  }, []);
 
   return (
     <>
-      <button className="text-sm text-primary w-fit" aria-modal onClick={(e) => {
-      e.preventDefault()
-      setOpen((open) => !open)
-      }}>
-        Press{" "}
-        <kbd className="pointer-events-none inline-flex h-5 select-none items-center gap-1 rounded border border-primary/10 bg-gradient-to-b from-primary-foreground to-primary/20 px-1.5 font-mono text-[10px] font-medium text-primary opacity-100" >
+      <button
+        className="text-sm text-primary w-fit"
+        aria-modal
+        onClick={(e) => {
+          e.preventDefault();
+          setOpen((open) => !open);
+        }}
+      >
+        Press{' '}
+        <kbd className="pointer-events-none inline-flex h-5 select-none items-center gap-1 rounded border border-primary/10 bg-gradient-to-b from-primary-foreground to-primary/20 px-1.5 font-mono text-[10px] font-medium text-primary opacity-100">
           <span className="text-xs">⌘</span>K
         </kbd>
       </button>
@@ -56,47 +58,67 @@ export function CommandDialogDemo() {
         <CommandList>
           <CommandEmpty>No results found.</CommandEmpty>
           <CommandGroup heading="Social">
-            <CommandItem onSelect={() => {window.open('https://x.com/ericvna')}}>
+            <CommandItem
+              onSelect={() => {
+                window.open('https://x.com/ericvna');
+              }}
+            >
               <Twitter className="mr-2 h-4 w-4" />
               <span>Twitter</span>
             </CommandItem>
-            <CommandItem onSelect={() => {window.open('https://github.com/ericviana')}}>
+            <CommandItem
+              onSelect={() => {
+                window.open('https://github.com/ericviana');
+              }}
+            >
               <Github className="mr-2 h-4 w-4" />
               <span>Github</span>
             </CommandItem>
-            <CommandItem onSelect={() => {window.open('https://bsky.app/profile/ericvna.bsky.social')}}>
+            <CommandItem
+              onSelect={() => {
+                window.open('https://bsky.app/profile/ericvna.bsky.social');
+              }}
+            >
               <Wand className="mr-2 h-4 w-4" />
               <span>Bluesky</span>
-            </CommandItem>
-            <CommandItem onSelect={() => {window.open('https://linkedin.com/in/eric-viana')}}>
-              <Linkedin className="mr-2 h-4 w-4" />
-              <span>Linkedin</span>
             </CommandItem>
           </CommandGroup>
           <CommandSeparator />
           <CommandGroup heading="Contact">
-            <CommandItem onSelect={() => {window.open('mailto:eric@crafta.studio')}}>
+            <CommandItem
+              onSelect={() => {
+                window.open('mailto:ericvswork@gmail.com');
+              }}
+            >
               <Mail className="mr-2 h-4 w-4" />
               <span>Email</span>
             </CommandItem>
-            <CommandItem onSelect={() => {window.open('https://cal.com/ericviana')}}>
+            <CommandItem
+              onSelect={() => {
+                window.open('https://cal.com/ericviana');
+              }}
+            >
               <Calendar className="mr-2 h-4 w-4" />
               Calendar
             </CommandItem>
           </CommandGroup>
           <CommandSeparator />
           <CommandGroup heading="Projects">
-            <CommandItem onSelect={() => {window.open('https://crafta.studio')}}>
-              <Sparkles className="mr-2 h-4 w-4" />
-              <span>Crafta</span>
-              <CommandShortcut>Software Agency</CommandShortcut>
-            </CommandItem>
-            <CommandItem onSelect={() => {window.open('https://fros.dev')}}>
+            <CommandItem
+              onSelect={() => {
+                window.open('https://fros.dev');
+              }}
+            >
               <TerminalSquare className="mr-2 h-4 w-4" />
               <span>Fros</span>
               <CommandShortcut>Open Source</CommandShortcut>
             </CommandItem>
-            <CommandItem className="mb-1" onSelect={() => {window.open('https://raunt.app')}}>
+            <CommandItem
+              className="mb-1"
+              onSelect={() => {
+                window.open('https://raunt.app');
+              }}
+            >
               <Workflow className="mr-2 h-4 w-4" />
               <span>Raunt</span>
               <CommandShortcut>Job Hunting SaaS</CommandShortcut>
@@ -105,5 +127,5 @@ export function CommandDialogDemo() {
         </CommandList>
       </CommandDialog>
     </>
-  )
+  );
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,13 @@
-import { ModeToggle } from "./components/toggle-theme";
-import { CommandDialogDemo } from "./components/command-demo";
-import { DraftingCompass, Palette, Code2, KeyboardIcon, Heart, Shell } from "lucide-react"
+import { ModeToggle } from './components/toggle-theme';
+import { CommandDialogDemo } from './components/command-demo';
+import {
+  DraftingCompass,
+  Palette,
+  Code2,
+  KeyboardIcon,
+  Heart,
+  Shell,
+} from 'lucide-react';
 
 export const metadata = {
   title: 'Eric Viana',
@@ -13,13 +20,19 @@ export const metadata = {
 
 export default function Home() {
   return (
-    <div className="antialiased h-[100dvh] p-4 bg-background squares">
+    <div className="bg-background squares h-[100dvh] p-4 antialiased">
       <div className="flex justify-between">
         <br />
         <ModeToggle />
       </div>
-      <div className="max-w-xl mx-auto mt-[30vh] flex flex-col gap-6 tracking-wide font-semibold">
-        <h1 className="text-primary/90 text-xl">I am Eric, a design engineer <DraftingCompass className="inline"/> dedicated to crafting meaningful <Shell className="inline" /> experiences. In the realm where technology and aesthetics  converge, I wield code <KeyboardIcon className="inline"/> and design <Palette className="inline"/> as my tools to breathe life into the intangible.</h1>
+      <div className="mx-auto mt-[30vh] flex max-w-xl flex-col gap-6 font-semibold tracking-wide">
+        <h1 className="text-primary/90 text-xl">
+          I am Eric, a design engineer <DraftingCompass className="inline" /> dedicated to
+          crafting meaningful <Shell className="inline" /> experiences. In the realm where
+          technology and aesthetics converge, I wield code{' '}
+          <KeyboardIcon className="inline" /> and design <Palette className="inline" /> as
+          my tools to breathe life into the intangible.
+        </h1>
         <CommandDialogDemo />
       </div>
     </div>
